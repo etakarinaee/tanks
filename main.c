9497219ae86dad0c37e3d5c0bc77ee55d09003d4
etakarinaee <godraylllll@gmail.com>
2026-02-21 17:56:35 +0300

    add GLEW linking; C89 complaince

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
+#include <stdbool.h>
 #include <stdio.h>
 
-#include <GL/glut.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 
 void display(void) {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -39,13 +41,15 @@ void reshape(int w, int h) {
 }
 
 int main(int argc, char **argv) {
+    GLenum err;
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(1280, 720);
     glutCreateWindow("Tanks");
 
     glewExperimental = true;
-    const GLenum err = glewInit();
+    err = glewInit();
     if (err != GLEW_OK) {
         fprintf(stderr, "glewInit: %s\n", glewGetErrorString(err));
         return -1;
