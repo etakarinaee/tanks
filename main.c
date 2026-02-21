cd6be86e3d2998f46b9dfa12cfb0e12bca0b986d
etakarinaee <godraylllll@gmail.com>
2026-02-21 22:45:59 +0300

    rename window

--- main.c
+++ main.c
@@ -122,7 +122,7 @@ int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(1280, 720);
-    glutCreateWindow("Tanks");
+    glutCreateWindow("Sausages");
 
     glewExperimental = true;
     err = glewInit();
